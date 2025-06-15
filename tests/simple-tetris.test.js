d08d12bbbdc533055d719af100c97da98623d10a
femon07 <femu.ren.gal@gmail.com>
2025-06-15 12:00:46 +0900

    テストファイルの整理: 重複・不要なテストファイルを削除

--- tests/simple-tetris.test.js
+++
@@ -1,9 +0,0 @@
-const { COLS, ROWS } = require('../src/constants/game');
-
-describe('Simple Tetris Test', () => {
-  test('should have correct board dimensions', () => {
-    console.log('Running simple test');
-    expect(COLS).toBe(10);
-    expect(ROWS).toBe(20);
-  });
-});
